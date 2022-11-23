2d6d1a265e1e7ac9f6b33d17db1473c09d1f9152
Matthew Carlson <matthewfcarlson+github@gmail.com>
2022-11-23 00:24:44 -0600

    tweaked the buttons a good bit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
       <p class="title"><span class="fw-300">Hello</span> {{ household_name }}</p>
       <p class="subtitle">Here's what's going on today</p>
     </section>
-    
+
     <article class="panel">
       <p class="panel-heading">
         Today's meal
@@ -14,20 +14,29 @@
         <a href="https://www.americastestkitchen.com/" target="_blank">Link to meal</a>
       </a>
       <div class="panel-block">
-        <button class="button is-warning">
-          <span class="icon is-left">
-            <i class="fas fa-forward" aria-hidden="true"></i>
-          </span>
-          <span>Skip This Meal</span>
-        </button>
-        <button class="button is-success">
-          <span class="icon is-left">
-            <i class="fas fa-check" aria-hidden="true"></i>
-          </span>
-          <span>We Made It</span>
-        </button>
+        <div class="buttons has-addons">
+
+          <button class="button is-warning">
+            <span class="icon is-left">
+              <i class="fa-solid fa-clock-rotate-left"></i>
+            </span>
+            <span>Make this another day</span>
+          </button>
+          <button class="button is-info">
+            <span class="icon is-left">
+              <i class="fas fa-forward" aria-hidden="true"></i>
+            </span>
+            <span>Ate something else</span>
+          </button>
+          <button class="button is-success">
+            <span class="icon is-left">
+              <i class="fas fa-check" aria-hidden="true"></i>
+            </span>
+            <span>Ate it</span>
+          </button>
+        </div>
       </div>
-    
+
     </article>
     <article class="panel">
       <p class="panel-heading">
@@ -37,26 +46,29 @@
         <p>
           Example task like clean the downstairs bathroom
         </p>
-        <button class="button is-warning">
-          <span class="icon is-left">
-            <i class="fas fa-forward" aria-hidden="true"></i>
-          </span>
-          <span>Skip This Meal</span>
-        </button>
-        <button class="button is-success">
-          <span class="icon is-left">
-            <i class="fas fa-check" aria-hidden="true"></i>
-          </span>
-          <span>We Made It</span>
-        </button>
+
+        <div class="buttons has-addons">
+          <button class="button is-warning">
+            <span class="icon is-left">
+              <i class="fas fa-forward" aria-hidden="true"></i>
+            </span>
+            <span>Done, Another!</span>
+          </button>
+          <button class="button is-success">
+            <span class="icon is-left">
+              <i class="fas fa-check" aria-hidden="true"></i>
+            </span>
+            <span>Cleaned it</span>
+          </button>
+        </div>
       </a>
       <div class="panel-block">
-        
+
       </div>
       <a class="panel-block is-active">
         Other people's tasks Example task like clean the downstairs bathroom
       </a>
-    
+
     </article>
     <article class="panel">
       <p class="panel-heading">
@@ -66,23 +78,26 @@
         Example simple 1hour or less task that's easy to do
       </a>
       <div class="panel-block">
-        <button class="button is-warning">
-          <span class="icon is-left">
-            <i class="fas fa-forward" aria-hidden="true"></i>
-          </span>
-          <span>I already did it, give me another</span>
-        </button>
-        <button class="button is-success">
-          <span class="icon is-left">
-            <i class="fas fa-check" aria-hidden="true"></i>
-          </span>
-          <span>I did It</span>
-        </button>
+        <div class="buttons has-addons">
+          <button class="button is-warning">
+            <span class="icon is-left">
+              <i class="fas fa-forward" aria-hidden="true"></i>
+            </span>
+            <span>Finished, Again!</span>
+          </button>
+          <button class="button is-success">
+            <span class="icon is-left">
+              <i class="fas fa-check" aria-hidden="true"></i>
+            </span>
+            <span>Did It</span>
+          </button>
+        </div>
       </div>
+
       <a class="panel-block is-active">
         The tasks that other people in the house have
       </a>
-    
+
     </article>
   </div>
 </template>
@@ -95,14 +110,14 @@ import { useUserStore } from "@/store";
 export default defineComponent({
   name: 'HomeView',
   components: {
-    
+
   },
   computed: {
-      household_name: function() {
-        const household = useUserStore().household;
-        if (household == null) return "";
-        return household.name;
-      },
+    household_name: function () {
+      const household = useUserStore().household;
+      if (household == null) return "";
+      return household.name;
+    },
   },
   data() {
     return {
