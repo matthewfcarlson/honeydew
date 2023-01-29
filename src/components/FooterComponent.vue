2b32824599628b04edc3c5ef817ff41969f72944
Matthew Carlson <matthewfcarlson+github@gmail.com>
2023-01-29 15:51:35 -0600

    Add version

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,11 @@
 <template>
     <footer class="footer">
-        <div class="content has-text-centered">
+        <div class="content has-text-centered is-bold text-bold">
             Made with Love By Matthew For Ellen
+            
+        </div>
+        <div class="has-text-centered text-small is-small">
+            Database Version 4
         </div>
     </footer>
 </template>
