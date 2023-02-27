ffeef372a71d458aee524928ae99af445da653bf
Ellen Carlson <59638391+ellenhc@users.noreply.github.com>
2023-02-26 19:23:52 -0600

    Issue link should open in new tab (#19)
    Fixes #12
    
    Co-authored-by: Ellen Carlson <ellenhc@users.noreply.github.com>

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -4,7 +4,7 @@
             Made with Love By Matthew For Ellen            
         </div>
         <div class="has-text-centered text-small is-small">
-            Have a problem? <a href="https://github.com/matthewfcarlson/honeydew/issues/new/choose">File an issue on Github</a>
+            Have a problem? <a href="https://github.com/matthewfcarlson/honeydew/issues/new/choose" target="_blank">File an issue on Github</a>
         </div>
     </footer>
 </template>
